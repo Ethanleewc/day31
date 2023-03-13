0e24a072111522b8f7374f2722b8750429074ec8
Ethanleewc <ethanleewc@gmail.com>
2023-03-13 20:05:31 +0800

    first commit

---
+++ src/app/components/count.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input, Output } from '@angular/core';
+import { Subject } from 'rxjs';
+
+@Component({
+  selector: 'app-count',
+  templateUrl: './count.component.html',
+  styleUrls: ['./count.component.css']
+})
+export class CountComponent {
+
+  @Input()
+  startNum = 0
+
+  //event output
+  @Output()
+  valueChanged = new Subject<number>()
+
+  updatePic(inc: number) {
+    this.startNum += inc
+    this.valueChanged.next(this.startNum)
+  }
+
+}
